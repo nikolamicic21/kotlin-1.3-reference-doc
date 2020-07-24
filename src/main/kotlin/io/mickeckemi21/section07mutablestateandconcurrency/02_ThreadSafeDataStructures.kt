1ed1817d37de6f060f6e7ac961ef22c18d7ad300
mickeckemi21 <mickeandroid21@gmail.com>
2020-07-24 20:42:43 +0200

    Section 07 Mutable State And Concurrency

---
+++ src/main/kotlin/io/mickeckemi21/section07mutablestateandconcurrency/02_ThreadSafeDataStructures.kt
@@ -0,0 +1,31 @@
+package io.mickeckemi21.section07mutablestateandconcurrency
+
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
+
+private var counter = AtomicInteger()
+
+private fun main() = runBlocking {
+    withContext(Dispatchers.Default) {
+        massiveRun {
+            counter.incrementAndGet()
+        }
+    }
+    println("Counter = $counter")
+}
+
+private suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 100
+    val k = 1000
+    val time = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) { action() }
+                }
+            }
+        }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
---
+++ src/main/kotlin/io/mickeckemi21/section07mutablestateandconcurrency/04_ThreadConfinementCoarseGrained.kt
@@ -0,0 +1,32 @@
+package io.mickeckemi21.section07mutablestateandconcurrency
+
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+private val counterContext = newSingleThreadContext("CounterContext")
+private var counter = 0
+
+private fun main() = runBlocking {
+    withContext(counterContext) {
+        massiveRun {
+            counter++
+        }
+    }
+
+    println("Counter = $counter")
+}
+
+private suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 100
+    val k = 1000
+    val time = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) { action() }
+                }
+            }
+        }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
