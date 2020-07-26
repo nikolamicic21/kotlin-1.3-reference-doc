3af77b0ea1e08f681cfd9eb2a1dcb0c72eca916b
mickeckemi21 <mickeandroid21@gmail.com>
2020-07-26 18:55:48 +0200

    Section 03 Equality

---
+++ src/main/kotlin/io/mickeckemi21/part07morelanguageconstructs/section03equality/00_StructuralVsReferentialEquality.kt
@@ -0,0 +1,9 @@
+package io.mickeckemi21.part07morelanguageconstructs.section03equality
+
+private fun main() {
+    val a = "TEST"
+    val b = "TEST"
+
+    println(a == b)
+    println(a === b)
+}
